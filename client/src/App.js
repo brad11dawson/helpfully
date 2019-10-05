01afb54b71051a9d72e7e71f1230d43208a9b0ab
brad11dawson <brad11dawson@gmail.com>
2019-10-05 17:21:03 -0600

    added bootstrap and header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
+import Header from './components/Header.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends React.Component {
@@ -7,20 +9,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
       </div>
     );
   }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
 
-function Header(props) {
-
-    
+function Header() {
+    return (
+        <Navbar bg="dark" expand="lg">
+            <Navbar.Brand>Helpfully</Navbar.Brand>
+        </Navbar>
+    )
 }
 
 export default Header;
