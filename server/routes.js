594c50232b730b670890ab875c72977786be7b96
joe <iwillridgley@gmail.com>
2019-10-06 10:47:11 -0600

    added cors

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,11 @@
 var express = require("express");
+var cors = require("cors");
 var router = express.Router();
 users = require("./routes/users");
 goals = require("./routes/goals");
 
-router.use(users).use(goals);
+router
+  .use(users)
+  .use(goals)
+  .use(cors());
 module.exports = router;
