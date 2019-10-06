395b20e6fd9a929590039958b598cf9a0a80266c
joe <iwillridgley@gmail.com>
2019-10-06 02:18:49 -0600

    edited proc file for heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import Header from './components/Header.js'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import SponsoredList from './components/SponsoredList.js';
-import NewPeopleList from './components/NewPeopleList.js';
-import LoginPage from './components/LoginPage.js';
+import React from "react";
+import Header from "./components/Header.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import SponsoredList from "./components/SponsoredList.js";
+import NewPeopleList from "./components/NewPeopleList.js";
+import LoginPage from "./components/LoginPage.js";
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       isLoggedIn: false
-    }
+    };
   }
 
   render() {
@@ -28,10 +28,8 @@ class App extends React.Component {
         <div className="App">
           <LoginPage />
         </div>
-      )
+      );
     }
-    
-    
   }
 }
 
