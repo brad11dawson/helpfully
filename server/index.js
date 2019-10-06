727e1cf7aed95b7c669eff66396e627554301e73
joe <iwillridgley@gmail.com>
2019-10-06 10:10:45 -0600

    changed order so cors policy was on top

--- server/index.js
+++ server/index.js
@@ -6,14 +6,15 @@ const Database = require("./database");
 const uuid = require("uuid/v4");
 
 express()
-  .use(express.static(path.join(__dirname, "public")))
-  .use(express.json())
   .use(cors())
   .all("/", function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
   })
+
+  .use(express.static(path.join(__dirname, "public")))
+  .use(express.json())
   .use(require("./routes"))
   .get("/", (req, res) => {
     res.status(200).json({ hello: "my name is Joe" });
